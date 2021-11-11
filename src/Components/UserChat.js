0e28cc7f879761ae34a8a5a5b964a37c61dd7f22
prasanthgajula08 <prasanth080898@gmail.com>
2021-11-11 17:43:13 -0500

    Init

---
+++ src/Components/UserChat.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export default function UserChat() {
+    return (
+        <div>
+            <div class="container">
+                <div style={{justifyContent:"center"}} class="row">
+                    <div style={{border:"1px solid rgba(39,41,43,0.1)", justifyContent:"center"}}>
+                        <div style={{height:"60px", border:"1px solid rgba(39,41,43,0.1)", justifyContent: "center", alignContent:"center"}} class="row">
+                            <div style={{justifyContent:"center", alignContent:"center"}} class="row">MessageBox</div>
+                        </div>
+                        <div style={{height:"866px", justifyContent:"center", alignContent:"center"}} class="row">ChatDisplay</div>
+                        <div style={{height:"50px", justifyContent:"center", alignContent:"center"}} class="row">
+                            <div class="input-group mb-3">
+                                <input type="text" class="form-control" placeholder="Type your message..." aria-label="Recipient's username" aria-describedby="button-addon2" />
+                                <button class="btn btn-outline-primary" type="button" id="button-addon2">Send</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import UserChat from './Components/UserChat';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route exact path='/' element={<UserChat/>}/>
+      </Routes>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
